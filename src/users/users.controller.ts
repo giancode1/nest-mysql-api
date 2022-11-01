c2b7bbb37f624e890cddb27385ab3629c6fc7989
Giancarlo Culcay <gieg5c@gmail.com>
2022-10-31 20:38:15 -0500

    lista de usuarios

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './user.entity';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
+  @Get()
+  getUsers(): Promise<User[]> {
+    return this.usersService.getUsers();
+  }
+
   @Post()
-  createUser(@Body() newUser: CreateUserDto) {
+  createUser(@Body() newUser: CreateUserDto): Promise<User> {
     return this.usersService.createUser(newUser);
   }
 }
